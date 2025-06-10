47d34de83fdec78605bbaa60df8843e86376b68a
Huizhong CHEN <hzchen0035@qq.com>
2025-06-10 09:04:37 +0800

    Added MATLAB functions: `Date2Days` and `Days2Date` to support conversion between dates and days

---
+++ MATLAB/Days2Date.m
@@ -0,0 +1,26 @@
+function date = Days2Date(days, year)
+    % Days2Date convert a specific day to the corresponding date in the year.
+    % Input:
+    %     days: the specific day
+    %     year: the corresponding year
+    % Output:
+    %     date: the corresponding date in the year
+    % Usage:
+    %     days = Days2Date(10, 2024)
+    %     return days = 1   10
+    %
+    % Code by Chen Huizhong, 2025-06-10.
+    if nargin ~= 2
+        error('Invalid input!');
+    end
+    daysOfMonth=[31,28,31,30,31,30,31,31,30,31,30,31];
+
+    if (mod(year, 4) == 0 && mod(year, 100) ~= 0) || mod(year, 400) == 0
+        daysOfMonth(2) = 29;
+    end
+
+    month = find(((days-cumsum(daysOfMonth))<=0) == 1, 1);
+    lag = sum(daysOfMonth(1:month)) - days;
+    day = daysOfMonth(month) - lag;
+    date = [month, day];
+end
---
+++ README.md
@@ -0,0 +1,7 @@
+# Toolkit
+
+Some widgets that I could utilize when coding. 
+
+## MATLAB
+
+Some convenient function in MATLAB.
